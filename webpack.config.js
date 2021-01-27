445e24460e90d0335a3e15d52281ff1e9803675c
Adan Ferguson <adanferguson@gmail.com>
2021-01-27 16:19:02 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/scripts/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(sass|css)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Demono',
+        }),
+        new CleanWebpackPlugin()
+    ],
+}
