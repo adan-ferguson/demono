0885a64a2808e8f2783574a920f2e5cba99ca585
Adan Ferguson <adanferguson@gmail.com>
2021-02-16 21:25:54 -0700

    configing

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         ],
     },
     resolve: {
-        extensions: [ '.tsx', '.ts', '.js', '.json' ],
+        extensions: [ '.tsx', '.ts', '.js' ],
     },
     output: {
         filename: 'bundle.js',
